3ee4f4b00cf2f9f988e0e5a51dd595bef628f222
Danilon6 <danilonebulosi@gmail.com>
2024-04-19 16:42:25 +0200

    Modified_position

--- M_1/S_1/G_5/PlayerMultimediale/src/it/epicode/interfaces/PuoRegolareLaLuminosita.java
+++
@@ -1,8 +0,0 @@
-package it.epicode.interfaces;
-
-public interface PuoRegolareLaLuminosita {
-    int luminosita = 0;
-
-    void alzaLuminosita();
-    void abbassaLuminosita();
-}
--- M_1/S_1/G_5/PlayerMultimediale/src/it/epicode/menu/MenuOption.java
+++
@@ -1,9 +0,0 @@
-package it.epicode.menu;
-
-public enum MenuOption {
-    MAIN_MENU_OPTION_1,
-    MAIN_MENU_OPTION_2,
-    SUB_MENU_2_OPTION_1,
-    SUB_MENU_2_OPTION_2,
-    SUB_MENU_2_OPTION_3
-}
